ddfb9cdad821c0cad492d5d6d4d9fdf18ceae53f
Tohid Hemmati <tohidhemmati.th@gmail.com>
2025-03-08 17:03:55 +0300

    Vite configured

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+RUN mkdir -p /var/www/html/storage/logs
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
+chmod -R 775 /var/www/html/public/build
+chown -R www-data:www-data /var/www/html/public/build
 php artisan config:cache
 
 php artisan migrate --force
 
 php artisan db:seed --force
-exec php-fpm
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
--- src/vite.config.js
+++ src/vite.config.js
@@ -1,28 +1,36 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: path.resolve(__dirname, 'public/build'),
+            rollupOptions: {
+      input: path.resolve(__dirname, 'resources/js/app.js'),
+    },
+    },
     plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+        vue(),
     ],
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    },
+    server: {
+        host: '0.0.0.0',
+        watch: {
+            usePolling: true,
+        },
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
         },
     },
 });
